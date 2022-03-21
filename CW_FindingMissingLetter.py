2efc4cea560aa08efcf40be95bb87b7e045441bb
Alexandra (Aleks) Hatfield <22735580+aleks-hat@users.noreply.github.com>
2022-03-21 18:57:07 -0400

    Update CW_FindingMissingLetter.py

--- CW_FindingMissingLetter.py
+++ CW_FindingMissingLetter.py
@@ -1,3 +1,12 @@
 #Solution to https://www.codewars.com/kata/5839edaa6754d6fec10000a2
 #Finding the Missing Letter, 6kyu Difficulty
 
+#Instructions:
+#Write a method that takes an array of consecutive (increasing) letters as input and that returns the missing letter in the array.
+
+#You will always get an valid array. And it will be always exactly one letter be missing. The length of the array will always be at least 2.
+#The array will always contain letters in only one case.
+
+#WORK IN PROGRESS
+
+
