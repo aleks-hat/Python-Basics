c6ee10ddc6fef2ddc8661ad6240033940a66d455
Alexandra (Aleks) Hatfield <22735580+aleks-hat@users.noreply.github.com>
2022-03-21 19:03:43 -0400

    added instructions

--- CW_Categorize New Member.py
+++ CW_Categorize New Member.py
@@ -1,4 +1,10 @@
 #CodeWars Categorize New Member Solution, Difficulty 7kyu
+
+#Instructions:
+#Input will consist of a list of pairs. Each pair contains information for a single potential member. Information consists of an integer for the person's age and an integer for the person's handicap.
+#Output will consist of a list of string values stating whether the respective member is to be placed in the senior or open category.
+
+
 # if the first and second index meet the senior criteria, return senior. else return open
 #Completion time: ~7 min
 
