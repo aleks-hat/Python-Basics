b136997ef38325fbeb1a0a9c6662bffc1bb9e04a
Alexandra (Aleks) Hatfield <22735580+aleks-hat@users.noreply.github.com>
2022-03-21 19:15:42 -0400

    minor typo

--- CW_ShortestWord.py
+++ CW_ShortestWord.py
@@ -1,4 +1,4 @@
-#Solution in reposnse to https://www.codewars.com/kata/57cebe1dc6fdc20c57000ac9
+#Solution in response to https://www.codewars.com/kata/57cebe1dc6fdc20c57000ac9
 #CodeWars Shortest Word, Level 7kyu Difficulty
 #Instructions:
 #Simple, given a string of words, return the length of the shortest word(s).
