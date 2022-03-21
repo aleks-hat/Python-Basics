2372e7b558dd2d2b44c1889052c2c5a15d8073f0
Alexandra (Aleks) Hatfield <22735580+aleks-hat@users.noreply.github.com>
2022-03-21 18:40:53 -0400

    Update CW_ShortestWord.py

--- CW_ShortestWord.py
+++ CW_ShortestWord.py
@@ -1,2 +1,8 @@
 #Solution in reposnse to https://www.codewars.com/kata/57cebe1dc6fdc20c57000ac9
 #CodeWars Shortest Word, Level 7kyu Difficulty
+#Instructions:
+#Simple, given a string of words, return the length of the shortest word(s).
+#String will never be empty and you do not need to account for different data types.
+
+#WORK IN PROGRESS
+
