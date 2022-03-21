065ccc9ed3ba97cdc0e03d7b54384bad644cb1e1
Alexandra (Aleks) Hatfield <22735580+aleks-hat@users.noreply.github.com>
2022-03-21 19:06:37 -0400

    added instructions

--- CW_Highest and Lowest.py
+++ CW_Highest and Lowest.py
@@ -1,4 +1,12 @@
 #Highest and Lowest Solution, Level 7kyu Difficulty
+
+#Instructions:
+#You are given a string of space separated numbers, and have to return the highest and lowest number.
+#Notes: 
+#All numbers are valid
+#There will always be at least one number in the input string.
+#Output string must be two numbers separated by a single space, and highest number is first.
+
 #for numbers given, if number is higher than known highest number
 # replace until highest num is found, then print - same with lowest number
 #Completion time: ~ 12 min
