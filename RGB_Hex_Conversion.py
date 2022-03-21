713d2bb90788ceb9b5c2a4218ad9664a4f1f5210
Alexandra (Aleks) Hatfield <22735580+aleks-hat@users.noreply.github.com>
2022-03-21 19:33:24 -0400

    added instructions

--- RGB_Hex_Conversion.py
+++ RGB_Hex_Conversion.py
@@ -1,2 +1,8 @@
 #Solution to https://www.codewars.com/kata/513e08acc600c94f01000001
 #CodeWars RGB to Hex Conversion, 5kyu Dificulty
+
+#Instructions:
+#The rgb function is incomplete. 
+#Complete it so that passing in RGB decimal values will result in a hexadecimal representation being returned.
+
+#WORK IN PROGRESS
