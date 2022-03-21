d14d5491c25e0f0c3cc2c160796189c91d27cacc
Alexandra (Aleks) Hatfield <22735580+aleks-hat@users.noreply.github.com>
2022-03-21 19:18:30 -0400

    added problem source

--- CW_Highest and Lowest.py
+++ CW_Highest and Lowest.py
@@ -1,4 +1,5 @@
-#Highest and Lowest Solution, Level 7kyu Difficulty
+#Solution to https://www.codewars.com/kata/554b4ac871d6813a03000035
+#CodeWars Highest and Lowest, Level 7kyu Difficulty
 
 #Instructions:
 #You are given a string of space separated numbers, and have to return the highest and lowest number.
