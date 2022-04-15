85aed198993c45de3dbd2f0e0b06ece1f896087d
Alexandra (Aleks) Hatfield <22735580+aleks-hat@users.noreply.github.com>
2022-04-15 19:10:27 -0400

    Create CW_expanded_form.py

---
+++ CW_expanded_form.py
@@ -0,0 +1,4 @@
+#Link to problem: https://www.codewars.com/kata/5842df8ccbd22792a4000245
+#Solution to Write Number in Expanded Form
+#Instructions:  For example: expanded_form(12) # Should return '10 + 2' , expanded_form(70304) # Should return '70000 + 300 + 4'
+
