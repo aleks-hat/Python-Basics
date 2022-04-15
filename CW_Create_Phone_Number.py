1181550ed3cdc73891c7c86b47a035c146142ec7
Alexandra (Aleks) Hatfield <22735580+aleks-hat@users.noreply.github.com>
2022-04-15 19:30:16 -0400

    Create CW_Create_Phone_Number.py

---
+++ CW_Create_Phone_Number.py
@@ -0,0 +1,3 @@
+#Link to problem: https://www.codewars.com/kata/525f50e3b73515a6db000b83
+#Solution to CodeWars Create Phone Number
+#Instructions: Write a function that accepts an array of 10 integers (between 0 and 9), that returns a string of those numbers in the form of a phone number.
