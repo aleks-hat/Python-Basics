5b0ac1e4573087708998728e6368acbf1ba16ce1
Alexandra (Aleks) Hatfield <22735580+aleks-hat@users.noreply.github.com>
2022-04-15 19:19:22 -0400

    Create CW_move_zeros.py

---
+++ CW_move_zeros.py
@@ -0,0 +1,3 @@
+#Link to problem: https://www.codewars.com/kata/52597aa56021e91c93000cb0
+#Solution to Moving Zeros to the End
+#Instructions: Write an algorithm that takes an array and moves all of the zeros to the end, preserving the order of the other elements.
