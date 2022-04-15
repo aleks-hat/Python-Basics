6e07db0a9e1f2b66e1165d9e5f6962d6f9c2ba23
Alexandra (Aleks) Hatfield <22735580+aleks-hat@users.noreply.github.com>
2022-04-15 16:32:13 -0400

    Update and rename counting_bits.py to CW_counting_bits.py

--- CW_counting_bits.py
+++ CW_counting_bits.py
@@ -3,3 +3,10 @@
 
 # Link to problem: https://www.codewars.com/kata/526571aae218b8ee490006f4/train/python
 #Solution for CodeWars Counting Bits problem 6kyu
+
+def count_bits(n):
+    count = 0
+    while(n):
+        count += n & 1
+        n >>= 1
+    return count
