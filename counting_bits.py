f32ab98d471a15f67a894154c602c154bdf6538c
Alexandra (Aleks) Hatfield <22735580+aleks-hat@users.noreply.github.com>
2022-04-15 16:31:33 -0400

    Create counting_bits.py

---
+++ counting_bits.py
@@ -0,0 +1,5 @@
+#Instructions: Write a function that takes an integer as input, and 
+#returns the number of bits that are equal to one in the binary representation of that number.
+
+# Link to problem: https://www.codewars.com/kata/526571aae218b8ee490006f4/train/python
+#Solution for CodeWars Counting Bits problem 6kyu
