32c0dab84670f1faae9e92b89c17c43bdd5770a6
Alexandra (Aleks) Hatfield <22735580+aleks-hat@users.noreply.github.com>
2022-04-15 19:10:54 -0400

    Update CW_expanded_form.py

--- CW_expanded_form.py
+++ CW_expanded_form.py
@@ -2,3 +2,13 @@
 #Solution to Write Number in Expanded Form
 #Instructions:  For example: expanded_form(12) # Should return '10 + 2' , expanded_form(70304) # Should return '70000 + 300 + 4'
 
+def expanded_form(num):
+    num = str(num)
+    index = []
+    
+    for i, j in enumerate(num[::-1]):
+        if int(j) != 0:
+            index.append(j + ('0' * i))
+
+    return ' + '.join(index[::-1])
+    pass
